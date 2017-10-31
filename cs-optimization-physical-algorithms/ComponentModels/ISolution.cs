14c8681e5ba1013b0294becde2e14ed6e7cacafa
xschen <xs0040@gmail.com>
2017-10-31 09:16:13 +0800

    source setup

---
+++ cs-optimization-physical-algorithms/ComponentModels/ISolution.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OPA.ProblemModels;
+
+namespace OPA.ComponentModels
+{
+    public interface ISolution
+    {
+        ISolution Clone();
+    }
+}
